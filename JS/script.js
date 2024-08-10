d2453cc964aa025b12eda9fcc114ba357b5c80df
FSFahim <farhanshahriar3728@gmail.com>
2024-08-10 21:23:40 +0600

    validation and remove book feature

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 //Get the UI elements
 let form = document.querySelector("#book-form");
+let bookList = document.querySelector("#book-list");
 
 //Book Class
 class Book {
@@ -30,10 +31,27 @@ class UI {
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
   }
+  showAlert(message, className) {
+    let div = document.querySelector(`.${className}`);
+    div.innerText = message;
+    div.style.display = "block";
+
+    setTimeout(() => {
+      div.style.display = "none";
+    }, 2000);
+  }
+  removeFromBookList(target) {
+    if (target.hasAttribute("href")) {
+      if (confirm("Are you sure?")) {
+        target.parentElement.parentElement.style.display = "none";
+      }
+    }
+  }
 }
 
 //Add event listeners
 form.addEventListener("submit", newBook);
+bookList.addEventListener("click", removeBook);
 
 //Define functions
 function newBook(element) {
@@ -41,11 +59,23 @@ function newBook(element) {
   let author = document.querySelector("#author").value;
   let isbn = document.querySelector("#isbn").value;
 
-  let book = new Book(title, author, isbn);
-
   let ui = new UI();
-  ui.addToBookList(book);
-  ui.clearField();
+
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("Please fill all the fields!", "error");
+  } else {
+    let book = new Book(title, author, isbn);
+
+    ui.addToBookList(book);
+    ui.clearField();
+    ui.showAlert("Book Added Successfully", "success");
+  }
 
   element.preventDefault();
+}
+
+function removeBook(e) {
+  let ui = new UI();
+  ui.removeFromBookList(e.target);
+  ui.showAlert("Book Removed Successfully", "success");
 }
